c2d6e3c1fd76a32362a503605350542bbccd170b
nyatinte <takumi.062757@gmail.com>
2024-01-21 20:53:05 +0900

    Active Link Styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,79 +1,88 @@
-import { ActionFunction, LinksFunction, LoaderFunction, json, redirect } from "@remix-run/node";
+import {
+  ActionFunction,
+  LinksFunction,
+  LoaderFunction,
+  json,
+  redirect,
+} from '@remix-run/node';
 import {
   Form,
-  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
   useLoaderData,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
-import appStylesHref from "./app.css"
-import { createEmptyContact, getContacts } from "./data";
+import appStylesHref from './app.css';
+import { createEmptyContact, getContacts } from './data';
 
-export const links:LinksFunction = () => (
-  [{rel: "stylesheet", href:appStylesHref},]
-)
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: appStylesHref },
+];
 
 export const loader = (async () => {
-  const contacts = await getContacts()
-  return json({contacts})
-}) satisfies LoaderFunction
+  const contacts = await getContacts();
+  return json({ contacts });
+}) satisfies LoaderFunction;
 
-export const action =(async () => {
-    const contact = await createEmptyContact()
-    return redirect(`/contacts/${contact.id}/edit`)
-}) satisfies ActionFunction
+export const action = (async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}) satisfies ActionFunction;
 
 export default function App() {
-  const {contacts} = useLoaderData<typeof loader>()
+  const { contacts } = useLoaderData<typeof loader>();
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
+        <div id='sidebar'>
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form id='search-form' role='search'>
               <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
+                id='q'
+                aria-label='Search contacts'
+                placeholder='Search'
+                type='search'
+                name='q'
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id='search-spinner' aria-hidden hidden={true} />
             </Form>
-            <Form method="post">
-              <button type="submit">New</button>
+            <Form method='post'>
+              <button type='submit'>New</button>
             </Form>
           </div>
           <nav>
-          {contacts.length ? (
+            {contacts.length ? (
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
                         </>
                       ) : (
                         <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? (
-                        <span>★</span>
-                      ) : null}
-                    </Link>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -84,8 +93,8 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
-          <Outlet/>
+        <div id='detail'>
+          <Outlet />
         </div>
 
         <ScrollRestoration />
