13dbdad80a26ed8669fba04e4783bfa2979a3b64
nyatinte <takumi.062757@gmail.com>
2024-01-21 21:03:49 +0900

    Deleting Records

---
+++ app/routes/contacts.$contactId_.destroy.tsx
@@ -0,0 +1,9 @@
+import { ActionFunction, redirect } from '@remix-run/node';
+import invariant from 'tiny-invariant';
+import { deleteContact } from '~/data';
+
+export const action = (async ({ params }) => {
+  invariant(params.contactId, 'Missing contactId param');
+  await deleteContact(params.contactId);
+  return redirect('/');
+}) satisfies ActionFunction;
